316a5efac8d6d00bb16539a138d304ff7c856906
wb_xiang.dai <wb_xiang.dai@rokid.com>
2017-09-24 18:55:10 +0800

    update

--- tools/test_channel_7_1.cpp
+++ tools/test_channel_7_1.cpp
@@ -9,12 +9,13 @@
 #include <stdlib.h> 
 
 #define MAX_SIZE 8192
+#define MIC_NUM 7
 
 int main(){
     int in, out, len, index = 0;
 
     umask(0);
-    if((in = open("/Users/daixiang/Downloads/6+1_16k_16bit.pcm", O_RDONLY)) > 0){
+    if((in = open("./6+1_16k_16bit.pcm", O_RDONLY)) > 0){
 
         long count = lseek(in, 0L, SEEK_END);
         lseek(in, 0L, SEEK_SET);
@@ -23,23 +24,28 @@ int main(){
         while((len = read(in, buff + index, MAX_SIZE)) > 0){
             index += len;
         }
-        short *buff_16 = (short *)buff;
 //        write(out, (void *)buff, count);
-        short **p = new short*[7];   
-        for(int i = 0; i < 7; i++)   
-            p[i] = new short[count / 2 / 7];   
+        short **p = new short*[MIC_NUM];   
+        for(int i = 0; i < MIC_NUM; i++)   
+            p[i] = new short[count / 2 / MIC_NUM];   
 
-        index = 0;
-        for(int i = 0; i < count / 2; i += 7){
-            for(int j = 0; j < 7; j++)
-                p[j][index] = buff_16[i + j];
-            index++;
-        }
+        short *buff_16 = (short *)buff;
+//      第一种
+        for(int i = 0; i < MIC_NUM; i++)
+            for(int j = 0; j < count / 2 / MIC_NUM; j++)
+                p[i][j] = buff_16[j * MIC_NUM + i];
+//      第二种
+//        index = 0;
+//        for(int i = 0; i < count / 2; i += MIC_NUM){
+//            for(int j = 0; j < MIC_NUM; j++)
+//                p[j][index] = buff_16[i + j];
+//            index++;
+//        }
         char path[64];
-        for(int i = 0; i < 7; i++){
-            sprintf(path, "/Users/daixiang/Downloads/16bit_%d.pcm", i);
-            if((out = open(path, O_CREAT | O_TRUNC | O_WRONLY, 0777)) > 0){
-                write(out, (void *)p[i], count / 7);
+        for(int i = 0; i < MIC_NUM; i++){
+            sprintf(path, "./16bit_%d.pcm", i + 1);
+            if((out = open(path, O_CREAT | O_TRUNC | O_WRONLY, 0666)) > 0){
+                write(out, (void *)p[i], count / MIC_NUM);
             }
         }
     }
